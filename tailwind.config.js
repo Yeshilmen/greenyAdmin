6a9cc53f756ed0e94eb1835ef211bbd32cd30f6e
yeshilmen <yeshilmen@gmail.com>
2022-10-26 01:00:45 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "media",
+  content: ["./**/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      body: ["Noto Sans", "sans-serif"],
+      logo: ["Fira Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
