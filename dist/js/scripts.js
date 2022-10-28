17b3254ea3945a0f1bef2af2ebeca614e22950a8
yeshilmen <yeshilmen@gmail.com>
2022-10-28 18:50:45 +0300

    Navbar hide/unhide function added

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,2 +1,12 @@
 const mobileIcon = document.querySelector("#mobile-menu-icon");
 const navbar = document.querySelector("#navbar");
+
+mobileIcon.addEventListener("click", () => {
+  let navbarPosition = navbar.offsetLeft;
+
+  if (navbarPosition == 0) {
+    navbar.style.marginLeft = "-320px";
+  } else {
+    navbar.style.marginLeft = "0px";
+  }
+});
