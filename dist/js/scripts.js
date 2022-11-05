065ddefdee3a7a700de7bda49a4692b25145b0a4
yeshilmen <yeshilmen@gmail.com>
2022-11-05 21:30:34 +0300

    Show active page in nav

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,3 +1,12 @@
+function ShowPageFilters() {
+  const showFilters = document.querySelector("#show-filters");
+  const filters = document.querySelector("#filters");
+
+  showFilters.addEventListener("click", () => {
+    filters.classList.toggle("hidden");
+  });
+}
+
 const mobileIcon = document.querySelector("#mobile-menu-icon");
 const navbar = document.querySelector("#navbar");
 
@@ -15,9 +24,9 @@ addEventListener("resize", () => {
   navbar.removeAttribute("style");
 });
 
-const showFilters = document.querySelector("#show-filters");
-const filters = document.querySelector("#filters");
-
-showFilters.addEventListener("click", () => {
-  filters.classList.toggle("hidden");
+window.addEventListener("load", () => {
+  let pagename = document.getElementById("pagename").dataset.name;
+  let datalink = document.querySelector('[data-link="' + pagename + '"]');
+  datalink.classList.add("active-nav-link");
+  console.log(datalink);
 });
